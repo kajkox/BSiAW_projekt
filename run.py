525844b19d6d0c4a966f5a5dd917ff63e2142d64
samuno717 <frymax717@gmail.com>
2026-01-05 13:03:16 +0100

    Implemented L7 security

--- run.py
+++ run.py
@@ -1,9 +1,32 @@
 from app import app, db
+from flask import Flask
+from flask_talisman import Talisman
+
+app = Flask(__name__)
+
+csp = {
+    'default-src': '\'self\'',
+    'img-src': '*',
+    'script-src': ['\'self\'', '\'unsafe-inline\'', 'https://*'],
+    'style-src': ['\'self\'', '\'unsafe-inline\'', 'https://*'],
+}
+
+Talisman(app,
+    content_security_policy=csp,
+    force_https=False,
+    session_cookie_secure=False,
+    strict_transport_security=False
+)
+
+db_password = os.environ.get('DB_PASSWORD')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://user:{db_password}@db/dbname'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if __name__ == '__main__':
     # Tworzenie tabel w bazie danych przy starcie, jeśli nie istnieją
     with app.app_context():
         db.create_all()
-    
+
+    debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
     # Uruchomienie serwera (host 0.0.0.0 jest wymagany dla Dockera)
-    app.run(host='0.0.0.0', port=5000, debug=True) # nosec B104
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode) # nosec B104
